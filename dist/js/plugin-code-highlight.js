59fc43970038a5fe942b4f9d7007158ebad085cc
Adam Quinton <quintonish@gmail.com>
2017-10-07 09:06:51 -0600

    syntax highlighting with ☔️🏹

--- dist/js/plugin-code-highlight.js
+++ dist/js/plugin-code-highlight.js
@@ -1,24 +1,14 @@
 document.addEventListener('postLoaded', function highlightCode() {
-  let url = '../../bower_components/rainbow/dist/rainbow.min.js';
+  let url = '../../dist/js/rainbow.min.js';
   var script = document.createElement('script');
   script.setAttribute('src', url);
   document.head.appendChild(script);
   var link = document.createElement('link');
   link.setAttribute('rel', 'stylesheet');
   link.setAttribute('type', 'text/css');
-  link.setAttribute('href', '../../bower_components/rainbow/themes/css/github.css');
+  link.setAttribute('href', '../../dist/css/rainbow.css');
   document.head.appendChild(link);
   script.onload = function() {
-    Rainbow.color();
+    Rainbow.color();/* global Rainbow */
   };
-  // let pres = [].slice.call(document.getElementsByClassName('pre'))
-  //   .forEach(function(pre) {
-  //     let preEl = document.createElement('video');
-  //     preEl.setAttribute('controls', '');
-  //     // preEl.setAttribute('preload', 'none');
-  //     let source = document.createElement('source');
-  //     source.setAttribute('src', pre.getAttribute('src'));
-  //     preEl.appendChild(source);
-  //     pre.appendChild(preEl);
-  //   });
 });
