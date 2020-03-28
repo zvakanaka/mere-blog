fe447d802a3f8b31ab9362ae539637c3d347b61b
zvakanaka <quintonish@gmail.com>
2020-03-27 22:59:42 -0600

    remove post event listener around plugin

--- src/js/plugin-avatar.js
+++ src/js/plugin-avatar.js
@@ -1,17 +1,15 @@
-document.addEventListener('postLoaded', function avatar() {
-  const usernameRegexArr = location.host.match(/^(.+).github.io/);
-  if (usernameRegexArr) {
-    const [, username] = usernameRegexArr;
-    const section = document.createElement('section');
-    section.classList.add('avatar-container');
-    const avatar = document.createElement('img');
-    // thanks!: https://stackoverflow.com/a/36380674/4151489
-    avatar.setAttribute('src', `https://github.com/${username}.png?size=200`);
-    avatar.setAttribute('alt', `GitHub Profile Picture for ${username}`);
-    avatar.classList.add('avatar');
-    section.appendChild(avatar);
-    const navigation = document.querySelector('.navigation');
-    // prepend avatar section
-    navigation.insertBefore(section, navigation.firstChild);
-  }
-});
+const usernameRegexArr = location.host.match(/^(.+).github.io/);
+if (usernameRegexArr) {
+  const [, username] = usernameRegexArr;
+  const section = document.createElement('section');
+  section.classList.add('avatar-container');
+  const avatar = document.createElement('img');
+  // thanks!: https://stackoverflow.com/a/36380674/4151489
+  avatar.setAttribute('src', `https://github.com/${username}.png?size=200`);
+  avatar.setAttribute('alt', `GitHub Profile Picture for ${username}`);
+  avatar.classList.add('avatar');
+  section.appendChild(avatar);
+  const navigation = document.querySelector('.navigation');
+  // prepend avatar section
+  navigation.insertBefore(section, navigation.firstChild);
+}
